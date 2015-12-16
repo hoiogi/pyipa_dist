804e0a8ed58501559e5fde2d2d7a1141d1a3940a
ChangWook Jeong <hoiogi@gmail.com>
2015-12-16 21:03:05 +0900

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+pyipa_dist
+~~~~~~~~
+
+:copyright: (c) 2015 ChangWook Jeong
+:license: see LICENSE for more details.
+
+"""
+
+__title__ = 'pyipa_dist'
+__version__ = '1.0.0'
+__description__ = 'This python module to creates plist, html file for Adhoc distribution'
+__url__ = 'https://github.com/hoiogi/pyipa_dist'
+__build__ = 1
+__author__ = 'ChangWook Jeong'
+__author_email__ = 'hoiogi@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 ChangWook Jeong'
+
+
+#from .IPAparser import IPAparser
+#from .bplist import BPlistReader, BPListWriter
